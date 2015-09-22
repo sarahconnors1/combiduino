6097fc7ccf2c7a75b40e6a0566e8afe050f65d52
sarahconnors1 <lboscagli@aol.com>
2015-09-22 11:19:10 +0200

    Create Bluetooth.ino

---
+++ Bluetooth.ino
@@ -0,0 +1,23 @@
+// ------------------------------------------------------
+//    GESTION DES FONCTIONS BLUETOOTH
+// -----------------------------------------------------
+
+void Send_to_BT(String str) {
+  
+  // envoie au debug sauf si info ECU RPM
+  if (!str.startsWith("ECU;") ){
+  debug("BT:"+str);
+  }
+  
+   for (int nr_car = 0; nr_car <= str.length(); nr_car++){ble_write( str[nr_car] );}
+   ble_do_events();
+  
+}
+void Read_BT(){
+   if ( ble_available() ){ 
+     while ( ble_available() )
+      inputString = inputString + char(ble_read());      
+    }
+    stringComplete = true;  
+ }
+  
