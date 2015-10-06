76366aa6ee619d9fcfd14b56af3b7a515f66950a
sarahconnors1 <lboscagli@aol.com>
2015-10-06 19:16:05 +0200

    Update Bluetooth.ino

--- Bluetooth.ino
+++ Bluetooth.ino
@@ -1,14 +1,13 @@
 // ------------------------------------------------------
 //    GESTION DES FONCTIONS BLUETOOTH
 // -----------------------------------------------------
-//
+
 void Send_to_BT(String str) {
   
   // envoie au debug sauf si info ECU RPM
   if (!str.startsWith("ECU;") ){
   debug("BT:"+str);
-  }
-  
+  } 
    for (int nr_car = 0; nr_car <= str.length(); nr_car++){ble_write( str[nr_car] );}
    ble_do_events();
   
@@ -16,8 +15,9 @@ void Send_to_BT(String str) {
 void Read_BT(){
    if ( ble_available() ){ 
      while ( ble_available() )
-      inputString = inputString + char(ble_read());      
+      inputString = inputString + char(ble_read());   
+    stringComplete = true;     
     }
-    stringComplete = true;  
+   
  }
   
