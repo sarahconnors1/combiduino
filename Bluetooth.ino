19050f8c46c7cbdf2c302a9c93ce275a55747152
sarahconnors1 <lboscagli@aol.com>
2015-10-17 12:56:59 +0200

    Update Bluetooth.ino

--- Bluetooth.ino
+++ Bluetooth.ino
@@ -5,10 +5,10 @@
 void Send_to_BT(String str) {
   
   // envoie au debug sauf si info ECU RPM
-  if (!str.startsWith("ECU;") ){
-  debug("BT:"+str);
-  } 
-   for (int nr_car = 0; nr_car <= str.length(); nr_car++){ble_write( str[nr_car] );}
+ if (!str.startsWith("ECU") ){
+  debug("BTSend:"+str);
+ } 
+   for (int nr_car = 0; nr_car <= str.length()+1; nr_car++){ble_write( str[nr_car] );}
    ble_do_events();
   
 }
@@ -16,7 +16,8 @@ void Read_BT(){
    if ( ble_available() ){ 
      while ( ble_available() )
       inputString = inputString + char(ble_read());   
-    stringComplete = true;     
+    stringComplete = true;
+    debug("BTRec:"+inputString);    
     }
    
  }
