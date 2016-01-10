1ca7ceb056a14560768e764b69149cf8720a9e3f
sarahconnors1 <lboscagli@aol.com>
2016-01-10 19:06:29 +0100

    Update gestionECU.ino

--- gestionECU.ino
+++ gestionECU.ino
@@ -3,12 +3,45 @@
 //-------------------------------------------------------------
 
 
+void calcul_carto(){
+  // calcul de la position actuelle dans la carto
+ int map_rpm_index = decode_rpm(engine_rpm_average);                      // retrouve index RPM
+ int map_pressure_index = decode_pressure(map_pressure_kpa);       // retrouve index pression
+ if ((map_rpm_index != point_RPM) or (map_pressure_index != point_KPA) ){
+  bin_carto_change = true;  
+ }
+  point_RPM = map_rpm_index;            // retrouve index RPM
+  point_KPA = map_pressure_index;       // retrouve index pression
+
+ // calcul du mode actuel du moteur
+  // Stopping 1
+  // Cranking  2
+  // Idling  3
+  // Running  10
+    
+  // check si le moteur tourne
+  if ( (micros() - pip_old) > 100000){engine_rpm_average = 0;}
+
+  running_mode = Running;
+  if (engine_rpm_average == 0){
+    running_mode = Stopping;
+  } else  if (engine_rpm_average < rev_mini){
+    running_mode = Cranking;
+  }
+
+// recalcul des nouvelles valeur avance et injection
+    calculdelavance();
+    calcul_injection();
+  
+}
+
 void calculdelavance(){
 // calcul du nombre de degrÃ© d'avance suivant la map
 // puis pour le saw du dÃ©lai en microseconds
 // puis en tick du timer (1 tick / 0,5 us)
   
-Degree_Avance_calcul = rpm_pressure_to_spark(engine_rpm_average, map_pressure_kpa);  
+Degree_Avance_calcul = ignition_map[point_KPA][point_RPM] + correction_degre ;  
+
 if (fixed == true){                                   
    map_value_us = 1536 - (25.6 * fixed_advance);
   }else{
@@ -20,12 +53,24 @@ if (fixed == true){
  tick = map_value_us * 2; // pour le timer  
 }
 
+
 // gestion de la nouvelle dépression
 void gestiondepression(){
-    map_pressure_kpa = map(analogRead(MAP_pin),0,correction_pressure,0,101);  // on converti la moyenne en KPA
-   MAP_accel = (previous_map_pressure_kpa - map_pressure_kpa) * MAP_check_per_S; // calcul en Kpa/S
+sum_pressure += analogRead(MAP_pin);
+count_pressure++;
+if (count_pressure >= nbre_mesure_pressure){
+   int kpa_moyen = sum_pressure / count_pressure;  
+   int kpa = map(kpa_moyen,0,correction_pressure,kpa_0V,101);  // on converti la moyenne en KPA   
+   count_pressure = 0;
+   sum_pressure = 0;
+   if (kpa != map_pressure_kpa ) {newvalue=true;}
+   map_pressure_kpa = kpa;
+   MAP_accel = (map_pressure_kpa - previous_map_pressure_kpa) * MAP_check_per_S; // calcul en Kpa/S
    previous_map_pressure_kpa =  map_pressure_kpa;
-   newvalue=true;
+
+  
+}   
+   
 }  
 
 
@@ -47,6 +92,7 @@ int decode_rpm(int rpm_) { // renvoi la valeur inférieur du bin
   return map_rpm;
 }
 //--------------------------------------------retrouve l index du tableau de la pression --------------------------------------------//
+#if VACUUMTYPE == 2
 int decode_pressure(int pressure_) { // renvoi la valeur inférieur du bin
    int map_pressure = 0;
    if(pressure_ < pressure_axis[0]){
@@ -60,26 +106,30 @@ int decode_pressure(int pressure_) { // renvoi la valeur inférieur du bin
    }
    return map_pressure;
 }
+#endif
 
-
-//-------------------------------------------- fonction pour retrouver le degree d avance a appliquer suivant  RPM et pression--------------------------------------------//
-int rpm_pressure_to_spark(int rpm, int pressure){
-  int table_value;
-  int map_rpm_index = decode_rpm(rpm);                      // retrouve index RPM
-  int map_pressure_index = decode_pressure(pressure);       // retrouve index pression
-    
-  table_value = ignition_map[map_pressure_index][map_rpm_index];
-  // correction de l avance manuel
-  table_value = table_value + correction_degre;
-  return table_value;
+#if VACUUMTYPE == 1
+int decode_pressure(int pressure_) { // renvoi la valeur inférieur du bin
+   int map_pressure = 0;
+   if(pressure_ > pressure_axis[0]){
+     map_pressure = 0;
+   }else if (pressure_ < pressure_axis[nombre_point_DEP -1]) {
+     map_pressure = nombre_point_DEP -1 ;
+   }else{
+     // retrouve la valeur inferieur 
+     while(pressure_ < pressure_axis[map_pressure]){map_pressure++;}
+   }
+   return map_pressure;
 }
+#endif
+
 
 //-------------------------------------------- Function to generate SAW signal to return to EDIS --------------------------------------------//
 //-------------------------------------------- PIP signal interupt --------------------------------------------// 
 void pip_interupt()  {
  //gestion plus simple des rpm moyens
 if ((digitalRead(interrupt_X) == LOW) and ( (micros() - pip_old) > debounce ) ) {   
-    pip_old = micros();pip_count++;
+    pip_old = micros();pip_count++;nbr_spark++;
     if (pip_count >= maxpip_count) {
         engine_rpm_average = (30000000 * maxpip_count) / (micros() - timeold ); 
         pip_count = 0;
@@ -113,8 +163,7 @@ unsigned int timeout_ignition = TCNT5 + tick;
 cylindre_en_cours++;
 if (cylindre_en_cours>4){cylindre_en_cours=1;}
 
-if (cylinder_injection[cylindre_en_cours - 1] == true){ // si on doit injecter
-  
+if ( (cylinder_injection[cylindre_en_cours - 1] == true)||(running_mode == Cranking)||(running_mode == Stopping) ) { // si on doit injecter ou tous les tours au demmarrage
   unsigned int timeout_injection = TCNT5 + tick_injection; 
   OCR5B = timeout_injection;            // compare match register 
   TIMSK5 |= (1 << OCIE5B);  // enable timer compare interrupt
